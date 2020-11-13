3a92b21dfc9b75912bbd65fab8bee88ed088caca
Tevon Patterson <Tevonpatt@gmail.com>
2020-11-13 15:11:10 -0800

    Validate JPA/Hibernate config

--- JPAArtistGallery/src/test/java/com/skilldistillery/artistgallery/entities/ArtistTest.java
+++ JPAArtistGallery/src/test/java/com/skilldistillery/artistgallery/entities/ArtistTest.java
@@ -1,6 +1,11 @@
 package com.skilldistillery.artistgallery.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -10,25 +15,35 @@ import org.junit.jupiter.api.Test;
 
 class ArtistTest {
 
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private static Artist artist;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("ArtistGalleryPU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		artist = em.find(Artist.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();artist = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_Artist_entity_mapping() {
+		assertNotNull(artist);
+		assertEquals("Vincent", artist.getName());
 	}
 
 }
