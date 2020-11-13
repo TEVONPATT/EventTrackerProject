b11221794e5a37c061c1d3b1a68df249808de98f
Tevon Patterson <Tevonpatt@gmail.com>
2020-11-13 15:42:33 -0800

    Stub Boot project with JPA project dependency

---
+++ ArtistGallery/src/main/java/com/skilldistillery/artistgallery/ArtistGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.artistgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistGalleryApplication.class, args);
+	}
+
+}
---
+++ ArtistGallery/src/test/java/com/skilldistillery/artistgallery/ArtistGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.artistgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
