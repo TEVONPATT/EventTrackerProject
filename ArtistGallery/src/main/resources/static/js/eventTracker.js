9e62e222450d9e264c2377828752a2be82d8b852
Tevon Patterson <Tevonpatt@gmail.com>
2020-11-20 16:36:13 -0800

    using VS Code FOR JS AND AJAX

---
+++ ArtistGallery/src/main/resources/static/js/eventTracker.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function () {
+  console.log("Script loaded");
+
+  init();
+});
+
+function init() {
+  console.log("in init()");
+  //TODO: set up event listeners
+}
+
+// xhr.open('PUT', 'api/artists/'+artistId);
+// xhr.send(JSON.stringify(updatedArtist));
+
+// xhr.open('Delete', 'api/artists/' + artistId);
